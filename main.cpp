9a9c78849436bad9eae852e1cc3dde5553747794
Aparecida Fernanda <aparecidafernandafroes123@gmail.com>
2020-05-08 19:18:04 -0300

    Relatorio gerado

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -6,6 +6,7 @@ int main()
 {
   string sexo;
   int cerveja, refri, espetos;
+  double consumo, couvert, ingresso, total;
   cout << "Sexo: ";
   cin>>sexo;
   cout<< "Quantidade de cervejas: ";
@@ -14,5 +15,32 @@ int main()
   cin >> refri;
   cout<< "Quantidade de espetinhos: ";
   cin >> espetos;
+
+  if (sexo == "F"){
+    ingresso = 8.0;
+  }
+  else {
+    ingresso = 10.00;
+  }
+   consumo = cerveja * 5.0 + refri * 3.0 + espetos * 7.0;
+  if(consumo>30.0){
+    couvert = 0.0;
+  }
+  else{
+    couvert = 4.0;
+  }
+  total = ingresso + consumo + couvert;
+  cout << fixed << setprecision(2);
+  cout<<endl<<"RELATORIO:" << endl;
+  cout << "Consumo = R$ "<< consumo << endl;
+  if(couvert==0.0){
+  cout<< "Isento de Couvert" << endl;}
+  else{
+    cout<<"Couvert = R$ " << couvert <<endl;
+  }
+  cout<<"Ingresso = R$ " << ingresso<< endl;
+  cout<< endl<<"Valor a pagar = R$ "<< total<< endl;
+
     return 0;
 }
+
